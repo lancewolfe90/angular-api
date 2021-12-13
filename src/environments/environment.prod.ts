00c4d93989908dfe9e52b6f02690465ebbcb1987
Lance Wolfe <lwolfe@talentpath.com>
2021-12-13 17:17:11 -0600

    Hopefully final copy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true
 };
+const firebaseConfig = {
+  apiKey: "AIzaSyB2u7bWDHYsjZV7MVBtrAjJQxJE6GXM2pI",
+  authDomain: "angular-api-85e7a.firebaseapp.com",
+  projectId: "angular-api-85e7a",
+  storageBucket: "angular-api-85e7a.appspot.com",
+  messagingSenderId: "513057928920",
+  appId: "1:513057928920:web:6f3d5d2c4dfed30d3389ad"
+};
